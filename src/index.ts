7e04b6b7731ccc776356190804c01970edd4f318
davidedotzero <larron959@gmaiil.com>
2025-10-16 16:20:51 +0700

    test date-fns-tz 2

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.listen(PORT, () => {
 });
 
 app.get('/', async (req, res) => {
-    res.send('hello human!!! o/: ' + new Date().toString() + " / " + formatInTimeZone(new Date(), "yyyy-MM-dd HH:mm:ss"));
+    res.send('hello human!!! o/: ' + new Date().toString() + " / " + formatInTimeZone(new Date(), "Asia/Bangkok", "yyyy-MM-dd HH:mm:ss"));
 });
 
 app.get('/api/ping', async (req, res) => {
